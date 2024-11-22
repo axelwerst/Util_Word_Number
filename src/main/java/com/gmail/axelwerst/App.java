7ac3ef8d22ff8b3ef6ff5d0783d6db6616c1b00f
Oleksandr Honcharenko <axelwerst@gmail.com>
2024-11-22 08:34:20 +0100

    1

---
+++ src/main/java/com/gmail/axelwerst/App.java
@@ -0,0 +1,19 @@
+package com.gmail.axelwerst;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+
+        List<String> words = Arrays.asList("apple", "cat", "banana", "pear");
+        String shortestWord = WordUtils.getShortestWord(words);
+        System.out.println("Найкоротше слово: " + shortestWord);
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        int sum = NumberUtils.getSum(numbers);
+        System.out.println("Сума чисел: " + sum);
+    }
+}
+
